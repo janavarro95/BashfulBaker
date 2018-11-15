6eb13af702a928c55b175e8c78d4db7a3915c325
Moises Martinez <mmart114@crm.ucsc.edu>
2018-11-15 03:07:57 -0800

    wrote code to drop cookie when player reaches neighbor's doormat, layered lower section of buildings, layered upper section of buildings

---
+++ BashfulBakerProject/Assets/Scripts/Objects/DropFood.cs
@@ -0,0 +1,40 @@
+﻿using Assets.Scripts.Collision;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
+
+namespace Assets.Scripts.Objects
+{
+    public class DropFood:MonoBehaviour{
+
+        public Tilemap tilemap;
+        CollisionChecker collision;
+        public TileBase dessert;
+        public void Start()
+        {
+            this.gameObject.AddComponent<CollisionChecker>();
+            this.collision = this.gameObject.GetComponent<CollisionChecker>();
+            this.collision.targetTag = "Player";
+        }
+
+        void Update()
+        {
+            if (collision.targetEntered)
+            {
+                Drop();
+            }
+        }
+
+        void Drop()
+        {
+            Vector3Int pos = new Vector3Int();
+            pos.Set((int) this.transform.position.x, (int)this.transform.position.y - 1, (int)this.transform.position.z);
+            tilemap.SetTile(pos, dessert);
+
+        }
+    }
+}
