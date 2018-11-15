c277eb5287ab837f7d526dce6ea90dff44b9ec20
Moises Martinez <mmart114@crm.ucsc.edu>
2018-11-14 16:27:27 -0800

    init

--- BashfulBakerProject/Assets/Scripts/Objects/WarpTile.cs
+++ BashfulBakerProject/Assets/Scripts/Objects/WarpTile.cs
@@ -1,4 +1,5 @@
-﻿using Assets.Scripts.Collision;
+﻿/*
+using Assets.Scripts.Collision;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,3 +31,4 @@ namespace Assets.Scripts.Objects
 
     }
 }
+*/
